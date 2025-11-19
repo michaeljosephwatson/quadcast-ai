e08c0f0d74ec9bd25530213dd24650de87db725e
michaeljosephwatson <michaeljosephwatson@yahoo.com>
2025-11-19 11:00:53 +0000

    Created the handler for the lambda function

--- add_podcast/handler.py
+++ add_podcast/handler.py
@@ -1,1 +1,24 @@
 """This file acts as the entry point for the add_podcast Lambda function."""
+
+import json
+from load import load_data_to_db_from_rss
+
+
+def lambda_handler(event, context):
+    """Entry point for the lambda function to add podcast data from an RSS feed to the database. 
+    Takes the RSS feed URL from the event body."""
+
+    body = json.loads(event['body'])
+    rss_url = body['podcast_url']
+
+    if rss_url:
+        load_data_to_db_from_rss(rss_url)
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Podcast data added successfully!')
+        }
+
+    return {
+        'statusCode': 400,
+        'body': json.dumps('Invalid request: podcast_url is required.')
+    }
