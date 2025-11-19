e41899af71de7d5f9e456f4a5df680143e0fb212
Zuhayr <zuhayrmohsen@gmail.com>
2025-11-19 16:49:46 +0000

    Refactored based on PR comments

--- dashboard/rds_queries.py
+++ dashboard/rds_queries.py
@@ -14,47 +14,45 @@ def get_rds_connection() -> connection:
         database=os.getenv("RDS_DB_NAME"),
         user=os.getenv("RDS_USERNAME"),
         password=os.getenv("RDS_PASSWORD"),
-        port=int(os.getenv("RDS_PORT")),
+        port=int(os.getenv("RDS_PORT", 5432)),
     )
     return conn
 
 
 def get_number_of_podcasts(conn: connection) -> int:
     """Returns the total number of podcasts in the database"""
-    cur = conn.cursor()
-    cur.execute("SELECT COUNT(*) FROM podcast;")
-    result = cur.fetchone()
-    cur.close()
+    with conn.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM podcast;")
+        result = cur.fetchone()
     return result[0]
 
 
 def get_number_of_episodes(conn: connection, podcast_name=None) -> int:
     """Returns the total number of episodes in the database or for a specific podcast"""
-    cur = conn.cursor()
-    if not podcast_name:
-        cur.execute("SELECT COUNT(*) FROM episode;")
-    else:
-        cur.execute("""
-            SELECT COUNT(*) FROM episode e
-            JOIN podcast p  ON e.podcast_id = p.podcast_id
-            WHERE p.podcast_name = %s;
-        """, (podcast_name,))
-    result = cur.fetchone()
-    cur.close()
+    with conn.cursor() as cur:
+        if not podcast_name:
+            cur.execute("SELECT COUNT(*) FROM episode;")
+        else:
+            cur.execute("""
+                SELECT COUNT(*) FROM episode e
+                JOIN podcast p  ON e.podcast_id = p.podcast_id
+                WHERE p.podcast_name = %s;
+            """, (podcast_name,))
+        result = cur.fetchone()
     return result[0]
 
 
 def get_number_of_transcripts(conn: connection, podcast_name=None) -> int:
     """Returns the total number of transcripts in the database or for a specific podcast"""
-    cur = conn.cursor()
-    if not podcast_name:
-        cur.execute("SELECT COUNT(*) FROM episode WHERE transcribed = TRUE;")
-    else:
-        cur.execute("""
-            SELECT COUNT(*) FROM episode e
-            JOIN podcast p  ON e.podcast_id = p.podcast_id
-            WHERE p.podcast_name = %s AND e.transcribed = TRUE;
-        """, (podcast_name,))
-    result = cur.fetchone()
-    cur.close()
+    with conn.cursor() as cur:
+        if not podcast_name:
+            cur.execute(
+                "SELECT COUNT(*) FROM episode WHERE transcribed = TRUE;")
+        else:
+            cur.execute("""
+                SELECT COUNT(*) FROM episode e
+                JOIN podcast p  ON e.podcast_id = p.podcast_id
+                WHERE p.podcast_name = %s AND e.transcribed = TRUE;
+            """, (podcast_name,))
+        result = cur.fetchone()
     return result[0]
