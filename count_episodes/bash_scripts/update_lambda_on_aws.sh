70c40c88aa8ff4586235dd1748af3ec6eede77cf
michaeljosephwatson <michaeljosephwatson@yahoo.com>
2025-11-21 15:22:29 +0000

    Completeed the lambda to count the number of untranscribed episodes

---
+++ count_episodes/bash_scripts/update_lambda_on_aws.sh
@@ -0,0 +1,3 @@
+./create_lambda_image.sh
+./push_lambda_image_to_aws.sh
+aws lambda update-function-code --function-name c20-quadcast-count-episodes --image-uri 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-quadcast-count-episodes-ecr:latest
