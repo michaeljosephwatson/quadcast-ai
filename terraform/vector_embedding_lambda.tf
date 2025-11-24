b254584a134b01169707a2382e8394c0c477fa32
helenacalvert <laurenhelenacalvert@yahoo.com>
2025-11-24 15:59:47 +0000

    removing unnecessary cloudwatch stuff

--- terraform/vector_embedding_lambda.tf
+++ terraform/vector_embedding_lambda.tf
@@ -113,32 +113,3 @@ resource "aws_lambda_function" "vector_embedding" {
     Environment = "dev"
   }
 }
-
-# CloudWatch Event Rule to trigger Vector Embedding Lambda
-resource "aws_cloudwatch_event_rule" "vector_embedding_trigger" {
-  name                = "c20-quadcast-vector-embedding-trigger"
-  description         = "Trigger vector embedding lambda every 5 minutes"
-  schedule_expression = "rate(5 minutes)"
-
-  tags = {
-    Name        = "c20-quadcast-vector-embedding-trigger"
-    Project     = "QuadCast"
-    Environment = "dev"
-  }
-}
-
-# CloudWatch Event Target
-resource "aws_cloudwatch_event_target" "vector_embedding_lambda" {
-  rule      = aws_cloudwatch_event_rule.vector_embedding_trigger.name
-  target_id = "VectorEmbeddingLambda"
-  arn       = aws_lambda_function.vector_embedding.arn
-}
-
-# Lambda Permission for CloudWatch Events
-resource "aws_lambda_permission" "allow_cloudwatch_vector_embedding" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.vector_embedding.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.vector_embedding_trigger.arn
-}
