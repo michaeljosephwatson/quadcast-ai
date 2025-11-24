0005755edcf875f0bc684a6e20d706ab53c810b2
helenacalvert <laurenhelenacalvert@yahoo.com>
2025-11-24 15:55:43 +0000

    removed vpc configs

--- terraform/vector_embedding_lambda.tf
+++ terraform/vector_embedding_lambda.tf
@@ -88,27 +88,20 @@ resource "aws_lambda_function" "vector_embedding" {
   timeout     = 900
   memory_size = 10240
 
-  vpc_config {
-    subnet_ids         = var.private_subnet_ids
-    security_group_ids = [aws_security_group.lambda_sg.id]
-  }
-
   environment {
     variables = {
-      DB_HOST         = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_HOST"]
-      DB_NAME         = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_DB_NAME"]
-      DB_USER         = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_USERNAME"]
-      DB_PASSWORD     = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_PASSWORD"]
-      DB_PORT         = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_PORT"]
-      S3_BUCKET       = "c20-quadcast-s3-bucket"
-      AWS_REGION      = "eu-west-2"
+      RDS_HOST         = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_HOST"]
+      RDS_DB_NAME      = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_DB_NAME"]
+      RDS_USERNAME     = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_USERNAME"]
+      RDS_PASSWORD     = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_PASSWORD"]
+      RDS_PORT         = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["RDS_PORT"]
+      S3_BUCKET       = aws_s3_bucket.quadcast_data.id
       OPENAI_API_KEY  = jsondecode(data.aws_secretsmanager_secret_version.quadcast_secrets.secret_string)["OPENAI_API_KEY"]
     }
   }
 
   depends_on = [
     aws_cloudwatch_log_group.vector_embedding_lambda,
-    aws_iam_role_policy_attachment.vector_embedding_lambda_vpc_execution,
     aws_iam_role_policy_attachment.vector_embedding_lambda_basic_execution,
     aws_iam_role_policy_attachment.vector_embedding_lambda_secrets_access,
     aws_iam_role_policy_attachment.vector_embedding_lambda_s3_access
@@ -116,22 +109,6 @@ resource "aws_lambda_function" "vector_embedding" {
 
   tags = {
     Name        = "c20-quadcast-vector-embedding"
-    Project     = "QuadCast"
-    Environment = "dev"
-  }
-}
-
-# ECR Repository for Vector Embedding Lambda
-resource "aws_ecr_repository" "vector_embedding_ecr" {
-  name                 = "c20-quadcast-vector-embedding"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-
-  tags = {
-    Name        = "c20-quadcast-vector-embedding-ecr"
     Project     = "QuadCast"
     Environment = "dev"
   }
